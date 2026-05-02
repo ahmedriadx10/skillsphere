f8c744ecfae7a841f41777bae70622b64c04ef0e
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-05-02 11:39:47 +0600

    all course page added

--- src/app/(public)/all-course/page.jsx
+++ src/app/(public)/all-course/page.jsx
@@ -1,10 +1,36 @@
+import { courseDataGet } from "@/utils/DataFetcher";
+import CourseCard from './../../../components/ui/CourseCard';
 
-const AllCourse = () => {
+const AllCourse = async() => {
+  
+  const courseData=await courseDataGet()
+
+console.log(courseData)
+  
   return (
-    <div>
+    <section className="max-w-7xl mx-auto w-[90%] py-20">
 
-   All Course Page   
-    </div>
+  <div className="text-center mb-16 space-y-4 max-w-xl w-[90%] mx-auto">
+
+<h4 className="text-4xl font-bold text-(--on-surface)">Expand Your Expertise</h4>
+<p className="text-(--textVariant)">Discover high-definition learning experiences designed to sharpen your skills and
+elevate your professional trajectory through expert-led content.</p>
+
+{/* search input here */}
+</div>
+
+{/* search input can be here dependend on layout structure */}
+
+
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+
+{courseData.map((course)=><CourseCard key={course.id} course={course} />)}
+
+
+</div>
+
+
+    </section>
   );
 };
 
---
+++ src/components/ui/CourseCard.jsx
@@ -0,0 +1,75 @@
+import { Avatar, Button, Chip } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import { FaRegClock, FaStar } from "react-icons/fa";
+import { RiBarChartFill } from "react-icons/ri";
+
+const CourseCard = ({course}) => {
+ 
+ const {id,title,instructor,rating,category,instructor_img,duration,price,image,description,level}=course
+
+  return (
+    <div className="rounded-3xl shadow flex flex-col justify-between gap-3 hover:-translate-y-1 hover:shadow-md transition-transform duration-150">
+
+<div className="relative rounded-t-2xl border h-60 w-full  overflow-hidden">
+<Image src={image} alt={title} fill  className="object-cover" loading="eager"       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"         />
+
+<Chip  className="backdrop-blur-3xl bg-linear-[90deg,#6B38D4_0%,#BA0035_100%] text-white absolute  right-2 top-2 uppercase tracking-widest  ">{category}</Chip>
+
+</div>
+
+
+
+{/* rating and price area */}
+
+<div className="flex justify-between items-center px-6">
+  <p className="flex items-center gap-1 text-orange-600 font-bold "><FaStar className="text-xl"/> <span>{rating===5.0?rating+'.0':rating}</span></p>
+
+<p className="flex items-center gap-1"><RiBarChartFill />{level}</p>
+
+  <h6 className="font-bold text-2xl text-(--primaryViolet)">${price}</h6>
+</div>
+
+{/* title and description area */}
+
+<div className=" space-y-2 px-6">
+
+<h3 className="text-xl text-(--on-surface) font-semibold">{title}</h3>
+<p className="line-clamp-2 text-(--textVariant)  leading-5">{description}</p>
+</div>
+
+
+
+{/* instructor and duration area */}
+
+<div className="flex justify-between flex-wrap gap-4 pb-5 px-6">
+
+<div className="flex gap-2 items-center">
+    {/* <Image src={instructor_img} alt={instructor} width={32} height={'32'} className="rounded-full aspect-square"/> */}
+      <Avatar>
+        <Avatar.Image alt={instructor} src={instructor_img} />
+        <Avatar.Fallback>{instructor[0].concat(instructor[1]).toUpperCase()}</Avatar.Fallback>
+      </Avatar>
+<p className="font-semibold text-[#334155]">{instructor}</p>
+</div>
+
+<p className="flex gap-1 items-center text-[#94A3B8]"> <FaRegClock /> {duration}</p>
+
+</div>
+
+
+{/* card action area */}
+
+<div className="mb-6  px-6">
+
+<Link href={`/course-details/${id}`}> <Button className={'w-full bg-linear-[90deg,#6B38D4_0%,#BA0035_100%] rounded-xl'} >View Details</Button> </Link>
+
+</div>
+
+
+
+    </div>
+  );
+};
+
+export default CourseCard;
