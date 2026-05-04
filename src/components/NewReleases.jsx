c3a07bd62503d81137b5dac1f2c3de2fd74f8035
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-05-04 23:43:57 +0600

    overall review done

--- src/components/NewReleases.jsx
+++ src/components/NewReleases.jsx
@@ -82,12 +82,12 @@ const NewReleases = () => {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 b items-center mt-16">
           
           {/* Left Side: Swiper with SkillSphere Themed Card */}
-          <div className="h-full max-w-70 md:max-w-75 w-[90%] mx-auto lg:mx-0 ">
+          <div className="h-full max-w-50 sm:max-w-60 md:max-w-75 w-[90%] mx-auto lg:mx-0 ">
       <Swiper
   effect={'cards'}
   grabCursor={true}
   modules={[EffectCards]}
-  className="mySwiper max-w-60 md:max-w-75 h-100"
+  className="mySwiper max-w-50 sm:max-w-60 md:max-w-75 h-100"
 
 >
 
